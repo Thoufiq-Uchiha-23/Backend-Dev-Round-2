efb082c11b8d8df69b8949dca18e25dc249521f3
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-11-08 23:48:06 +0530

    express update

--- MEN are Brave/Express/app.js
+++ MEN are Brave/Express/app.js
@@ -1,8 +1,41 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express(); // opening the toolbox by storing all things in app variable
 
 app.set('view engine', 'ejs')
 
+// Middleware
+// Middleware runs default for all routes
+// Middleware are of 3 types: 
+// Built-in Middleware
+// Custom Middleware
+// Third-party Middleware : morgan package
+
+// Third-party Middleware
+app.use(morgan('dev'))
+
+// Custom middleware
+// This middleware as default runs for all routes...
+// app.use((req, res, next) => {
+//     console.log("this is middleware")
+
+//     const a = 5;
+//     const b = 3
+//     console.log(a + b)
+//     return next()
+// })
+
+// ... but we can also make middleware for particular routes
+app.get('/', (req, res, next) => {
+    const a = 5;
+    const b = 10;
+    console.log(a + b)
+    console.log('Welcome to home')
+    next()
+}, (req, res) => {
+    res.render('index')
+})
 app.get('/', (req, res) => {
     res.render('index')
 })
