783a22e1862101057b0efc1e644c7df8054aac19
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-11-09 20:04:33 +0530

    Middlewares with form handling done

--- MEN are Brave/Express/app.js
+++ MEN are Brave/Express/app.js
@@ -1,19 +1,23 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require("express");
+const morgan = require("morgan");
 
-const app = express(); // opening the toolbox by storing all things in app variable
+const app = express(); // opening the toolbox by storing all things in app 
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 // Middleware
 // Middleware runs default for all routes
-// Middleware are of 3 types: 
+// Middleware are of 3 types:
 // Built-in Middleware
 // Custom Middleware
 // Third-party Middleware : morgan package
 
 // Third-party Middleware
-app.use(morgan('dev'))
+app.use(morgan("dev"));
+ 
+// Built-in Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 // Custom middleware
 // This middleware as default runs for all routes...
@@ -27,28 +31,37 @@ app.use(morgan('dev'))
 // })
 
 // ... but we can also make middleware for particular routes
-app.get('/', (req, res, next) => {
-    const a = 5;
-    const b = 10;
-    console.log(a + b)
-    console.log('Welcome to home')
-    next()
-}, (req, res) => {
-    res.render('index')
-})
-app.get('/', (req, res) => {
-    res.render('index')
-})
+// app.get('/', (req, res, next) => {
+//     const a = 5;
+//     const b = 10;
+//     console.log(a + b)
+//     console.log('Welcome to home')
+//     next()
+// }, (req, res) => {
+//     res.render('index')
+// })
+// app.get('/', (req, res) => {
+//     res.render('index')
+// })
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/about', (req, res) => {
-    res.send('About Page')
-})
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
 
+app.get("/profile", (req, res) => {
+  res.send("Profile Page");
+});
 
-app.get('/profile', (req, res) => {
-    res.send('Profile Page')
-})
+// Built in controller used for Form and server operations
+// we have to create a specified key of route
+app.post("/get-form-data", (req, res) => {
+  console.log(req.body);
+  res.send("data received");
+});
 
-app.listen(3000)
+app.listen(3000);
 // to render html using express we use ejs, pug, chat
 // to setup ejs use this command -> npm i ejs
